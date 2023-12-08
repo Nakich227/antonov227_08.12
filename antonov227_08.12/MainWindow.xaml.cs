d82562e86e83c8055286e2f2200f162944e9a8fc
danil <dddd@gmail.com>
2023-12-08 11:33:30 +0300

    Добавьте файлы проекта.

---
+++ antonov227_08.12/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace antonov227_08._12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
+
+        private void BtnOK_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int k = -1;
+                int n = Convert.ToInt32(ListBoxData.Items[0]);
+                int max = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    int m = Convert.ToInt32(ListBoxData.Items[i]);
+                    if (m % 10 == 4 && m % 3 == 0)
+                    {
+                        if (k == -1)
+                        {
+                            max = m;
+                            k = 0;
+                        }
+                        else
+                        {
+                            max = max + m;
+                        }
+                    }
+                }
+                if (k != -1)
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n{max}";
+                    ListBoxData.Items.Add("0");
+                }
+                else
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n Чисел оканчивающихся на 4 нет";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnCancel_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
